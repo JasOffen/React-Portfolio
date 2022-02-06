d9787027c32000a28c8b9c3defd96462ea719003
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-02-05 20:18:08 -0700

    Added README

---
+++ README.md
@@ -0,0 +1,15 @@
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity) ![Maintainer](https://img.shields.io/badge/maintainer-Jason%20Offenbaker-purple) [![Website shields.io](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](http://shields.io/) [![Generic badge](https://img.shields.io/badge/One%20Word%20Description-Portfolio-<COLOR>.svg)](https://shields.io/) [![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+#Jason Offenbakers Profile(React Edition)
+***
+###Packages and Libraries used
+* JSX
+* JavaScript
+* CSS(UIKit)
+###Resources that can be found on this page
+* Github Links
+* Prior Projects
+* Group Projects
+* Resume
+* Social Medias
+###Contribution
+* Jason Offenbaker
--- react-portfolio/src/components/Projects/index.js
+++ react-portfolio/src/components/Projects/index.js
@@ -4,54 +4,54 @@ function Projects(){
 
     return(
         <section id="Projects">
-             <div class="uk-child-width-expand@s uk-text-center" uk-grid id="smallCards">
-                <div>
-                    <a href="https://jasoffen.github.io/WeatherApplication/" target="_blank">
-                        <img src="./assets/images/WeatherApplication.png" />
-                        <div class="uk-card uk-card-default uk-card-body">5 day weather forecast</div>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://jasoffen.github.io/WorkDaySchedular/" target="_blank">
-                        <img src="./assets/images/WorkCalendar.png" />
-                        <div class="uk-card uk-card-default uk-card-body">Daily Planner with average working hours of
-                            9-5
-                        </div>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://jasoffen.github.io/Code-Quiz/" target="_blank">
-                        <img src="../public/assets/images/CodeQuiz.png" />
-                        <div class="uk-card uk-card-default uk-card-body">This is a 6 Question coding challenge.</div>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://jasoffen.github.io/Password-Generator/" target="_blank">
-                        <img src="./assets/images/PasswordGenerator.png" />
-                        <div class="uk-card uk-card-default uk-card-body">Generates a password based on user input</div>
-                    </a>
-                </div>
+        <div class="uk-child-width-expand@s uk-text-center uk-grid" uk-grid="" id="smallCards">
+            <div class="uk-first-column">
+                <a href="https://jasoffen.github.io/WeatherApplication/" target="_blank">
+                    <img src="./assets/images/WeatherApplication.png"/>
+                    <div class="uk-card uk-card-default uk-card-body">5 day weather forecast</div>
+                </a>
             </div>
-            <div class="uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <a href="./old/index.html" target="_blank">
-                        <img src="./assets/images/old-portfolio.png" />
-                        <div class="uk-card uk-card-default uk-card-body">
-                            <p>First attempt at a portfolio</p>
-                        </div>
-                    </a>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body"></div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body"></div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body"></div>
-                </div>
+            <div>
+                <a href="https://jasoffen.github.io/WorkDaySchedular/" target="_blank">
+                    <img src="./assets/images/WorkCalendar.png"/>
+                    <div class="uk-card uk-card-default uk-card-body">Daily Planner with average working hours of
+                        9-5
+                    </div>
+                </a>
             </div>
-        </section>
+            <div>
+                <a href="https://jasoffen.github.io/Code-Quiz/" target="_blank">
+                    <img src="./assets/images/CodeQuiz.png"/>
+                    <div class="uk-card uk-card-default uk-card-body">This is a 6 Question coding challenge.</div>
+                </a>
+            </div>
+            <div>
+                <a href="https://jasoffen.github.io/Password-Generator/" target="_blank">
+                    <img src="./assets/images/PasswordGenerator.png"/>
+                    <div class="uk-card uk-card-default uk-card-body">Generates a password based on user input</div>
+                </a>
+            </div>
+        </div>
+        <div class="uk-child-width-expand@s uk-text-center uk-grid" uk-grid="">
+            <div class="uk-first-column">
+                <a href="./old/index.html" target="_blank">
+                    <img src="./assets/images/old-portfolio.png"/>
+                    <div class="uk-card uk-card-default uk-card-body">
+                        <p>First attempt at a portfolio</p>
+                    </div>
+                </a>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body"></div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body"></div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body"></div>
+            </div>
+        </div>
+    </section>
     );
 }
 
